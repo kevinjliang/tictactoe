b9d7da7c00eb2392a6a3ef11b54ea0f72541e4db
Kevin Liang - Surface <kevinjliang2011@gmail.com>
2016-07-11 15:01:33 -0400

    Test that didn't work

--- theanoTesting.py
+++ theanoTesting.py
@@ -6,17 +6,29 @@ Created on Sat Jul  9 16:07:26 2016
 @author: kevin_000
 """
 
+import tictactoe as ttt
 import theano
 import theano.tensor as T
 from theano import pp
 
-x = T.dscalar('x')
-y = T.dscalar('y')
-z = x**2 + 3*x*y
+agent = ttt.deepAI()
 
-dzdx = T.grad(z,x)
-pp(dzdx)
+x = T.matrix('x') 
+y = T.ivector('y')
 
-f = theano.function([x,y],dzdx)
-print(f(2,4))
-print(f(7,9))
+x = x.reshape((1,1,125,125))
+
+p_move, move = agent.net.forward(x,(1,1,125,125))
+
+game = ttt.tttGrid()
+
+image = game.getImage()
+image = image.reshape((1,1,125,125))
+
+makeMove = theano.function(
+    x,
+    move,
+)
+
+out = makeMove(image)
+print(out)
