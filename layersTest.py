23e3f771afe954ae775b434da86065446277e1a5
Kevin Liang - Surface <kevinjliang2011@gmail.com>
2016-08-29 14:20:07 -0400

    Tried creating layers from scratch. About to try creating a specific theano function for feedforward

---
+++ layersTest.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 16 16:29:21 2016
+
+@author: kevin_000
+
+Layer testing
+
+"""
+
+import theano
+from theano import tensor as T
+import layers
+
+
+datasets = layers.load_data('mnist.pkl.gz')
+
+
+train_set_x, train_set_y = datasets[0]
+valid_set_x, valid_set_y = datasets[1]
+test_set_x, test_set_y = datasets[2]
+
+x = T.matrix('x')  # data, presented as rasterized images
+y = T.ivector('y')  # labels, presented as 1D vector of [int] labels
+
+classifier = layers.LogisticRegression(n_in=28 * 28, n_out=10)
+
+predict_y = theano.function(
+    inputs=[x],
+    outputs=classifier.forward(x)
+)
+
+cost = classifier.negative_log_likelihood(y)
+
+g_W = T.grad(cost=cost, wrt=classifier.W)
+g_b = T.grad(cost=cost, wrt=classifier.b)
+
+learning_rate = 0.1
+updates = [(classifier.W, classifier.W - learning_rate * g_W),
+           (classifier.b, classifier.b - learning_rate * g_b)]
+           
+           
+y_predictions = predict_y(train_set_x.get_value())   
+
+
+
+
+
+
+
+
+
+
+
