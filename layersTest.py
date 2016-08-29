3d489255ae2308b6fecce2d84ec4d13a0b1b15af
Kevin Liang - Surface <kevinjliang2011@gmail.com>
2016-08-29 15:55:02 -0400

    MLP code now running. Dubious as to whether it's working. Predictions (feed-forward) is really slow for some reason

--- layersTest.py
+++ layersTest.py
@@ -11,7 +11,10 @@ Layer testing
 import theano
 from theano import tensor as T
 import layers
+import numpy as np
+import time
 
+startTime = time.time()
 
 datasets = layers.load_data('mnist.pkl.gz')
 
@@ -23,34 +26,59 @@ test_set_x, test_set_y = datasets[2]
 x = T.matrix('x')  # data, presented as rasterized images
 y = T.ivector('y')  # labels, presented as 1D vector of [int] labels
 
-classifier = layers.LogisticRegression(input=x,n_in=28 * 28, n_out=10)
+rng = np.random.RandomState(1337)
 
-cost = classifier.negative_log_likelihood(y)
+classifier = layers.MLP(
+        rng=rng,
+        input=x,
+        n_in=28 * 28,
+        n_hidden=500,
+        n_out=10
+    )
+    
+L1_reg=0.00
+L2_reg=0.0001
 
-g_W = T.grad(cost=cost, wrt=classifier.W)
-g_b = T.grad(cost=cost, wrt=classifier.b)
+cost = (
+    classifier.negative_log_likelihood(y)
+    + L1_reg * classifier.L1
+    + L2_reg * classifier.L2_sqr
+)
 
-learning_rate = 0.1
-updates = [(classifier.W, classifier.W - learning_rate * g_W),
-           (classifier.b, classifier.b - learning_rate * g_b)]
-           
-           
+gparams = [T.grad(cost, param) for param in classifier.params]
+
+print("Gradients calculated")
+print(time.time()-startTime)
+
+learning_rate = 0.01
+updates = [
+    (param, param - learning_rate * gparam)
+    for param, gparam in zip(classifier.params, gparams)
+]
+
+print("Starting predictions")
+print(time.time()-startTime)
 y_predictions = classifier.forward(train_set_x.get_value())   
+print("Predictions made")
+print(time.time()-startTime)
 
 train_model = theano.function(
     inputs=[],
     outputs=cost,
     updates=updates,
     givens={
-        x: train_set_x.get_value(),
-        y: train_set_y
+        x: train_set_x.get_value()[0:20],
+        y: train_set_y[0:20]
     }
 )
 
+print("Training...")
+print(time.time()-startTime)
 for it in range(25):
     iterCost = train_model()
     
     print(iterCost)
+    print(time.time()-startTime)
 
 
 
